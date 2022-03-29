21ad8f2450819b665ec1acd4c3747fb2e8780839
Nur Uddin <nuruddin46bd@gmail.com>
2022-03-29 12:42:47 +0600

    first commit

---
+++ src/Components/About/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>This is about us</h1>
+      <p>
+        <small>taile age biriyani cholbe</small>
+      </p>
+    </div>
+  );
+};
+
+export default About;
